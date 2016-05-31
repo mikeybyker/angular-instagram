aba2e595e1889fbb99cd079dded569094012333c
Mike Doran <mikeybyker@gmail.com>
2016-05-31 12:05:29 +0100

    Adding karma launchers

--- karma.conf.js
+++ karma.conf.js
@@ -65,6 +65,8 @@ module.exports = function(config) {
 
     plugins : [
       'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
       'karma-angular-filesort',
       'karma-coverage',
       'karma-jasmine',
