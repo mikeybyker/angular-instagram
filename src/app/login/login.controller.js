859c968a17822312c0735931902d94ba7aa23986
Mike <mikeybyker@gmail.com>
2016-08-28 13:05:27 +0100

    Start handling auth0 breaking changes

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -28,12 +28,14 @@
                 // let the authProvider > loginSuccess do the rest...
                 // OR THIS? NOTE: TEST SPEC > CHANGE TO SUIT
             }, function(err) {
-                $log.log('Error :(', err);
+                $log.warn('Error :(', err);
             });
         }
         var saveUserInfo = function(profile, token) {
             store.set('profile', profile);
             store.set('token', token);
+            // @WARN : No more access_token. Serverless not possible with auth0, sadly.
+            // https://auth0.com/docs/migrations
             store.set('access_token', profile.identities[0].access_token);
         }
 
