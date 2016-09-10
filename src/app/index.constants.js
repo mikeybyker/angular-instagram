b2cb4ef2dd4add4081709c097d74214aa24aa0eb
Mike Doran <mikeybyker@gmail.com>
2016-09-10 09:09:31 +0100

    docs update [ci skip]

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -14,7 +14,7 @@
         )
         // auth0 stopped (August 2016) sending the IdP access_token : need a backend proxy
         // A webtask will do it...
-        // See readme for webtask info...else write your backend code to do the same
+        // See README for webtask info...else write your backend code to do the same
         .constant('webtask',
             {
                 api: 'YOUR_WEBTASK_URL'
